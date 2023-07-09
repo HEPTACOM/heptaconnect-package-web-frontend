632886f488fe3ae8a63416e19df3052e5a7e7c3e
Joshua Behrens <behrens@heptacom.de>
2023-07-09 18:16:40 +0200

    Make notification attachment aware to allow more data

--- src/Components/Notification/Notification.php
+++ src/Components/Notification/Notification.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Package\WebFrontend\Components\Notification;
 
-final class Notification
+use Heptacom\HeptaConnect\Dataset\Base\AttachmentCollection;
+use Heptacom\HeptaConnect\Dataset\Base\Contract\AttachmentAwareInterface;
+use Heptacom\HeptaConnect\Dataset\Base\Support\AttachmentAwareTrait;
+
+final class Notification implements AttachmentAwareInterface
 {
+    use AttachmentAwareTrait;
+
     public function __construct(
         private string $type,
         private string $message,
     ) {
+        $this->attachments = new AttachmentCollection();
     }
 
     public function getType(): string
